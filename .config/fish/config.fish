924fa619a51779faa1ea69c1744d2b8366179b95
Karina Shedlbauer <k@rina.wtf>
2020-03-29 20:02:58 +0300

    Update htop and fish configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,5 @@
 screenfetch
 
-set -gx PATH /Library/Java/JavaVirtualMachines/graalvm-ee-20.0.0/Contents/Home/bin $PATH
+# set -gx PATH /Library/Java/JavaVirtualMachines/graalvm-ee-java11-20.0.0/Contents/Home/bin $PATH
+set -gx GRAALVM_HOME /Library/Java/JavaVirtualMachines/graalvm-ee-java11-20.0.0/Contents/Home
+
