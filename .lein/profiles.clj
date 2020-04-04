0ab35a3f558d6b25d4867f05581b1d992797bbcb
Karina Shedlbauer <k@rina.wtf>
2020-04-04 13:43:03 +0300

    Update leiningen config

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,5 @@
 {:user {:plugins [[lein-binplus "0.6.6"]
-                  [lein-jlink "0.2.1"]]}}
+                  [lein-jlink "0.2.1"]
+                  [cider/cider-nrepl "0.22.1"]]
 
+        :dependencies [[cljfmt "0.5.7"]]}}
