e793d6d436745d9f2e93cabcf5e93cde3f125514
Karina Shedlbauer <k@rina.wtf>
2020-12-28 06:27:28 +0300

    Update fish shell configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,11 @@
 if status is-interactive
- # neofetch
+  neofetch
 end
 
-neofetch
+# Setup nix package manager
+fenv source ~/.nix-profile/etc/profile.d/nix.sh
 
 # set -gx PATH /Library/Java/JavaVirtualMachines/graalvm-ee-java11-20.0.0/Contents/Home/bin $PATH
 set -gx GRAALVM_HOME /Library/Java/JavaVirtualMachines/graalvm-ee-java11-20.0.0/Contents/Home
 
+set -gx PATH ~/.local/bin $PATH
