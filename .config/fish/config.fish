303e6184816f785a1ee5d993bf37264f51c426c0
Karina Shedlbauer <k@rina.wtf>
2021-04-30 01:54:39 +0300

    Update configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,4 +8,4 @@ fenv source ~/.nix-profile/etc/profile.d/nix.sh
 # set -gx PATH /Library/Java/JavaVirtualMachines/graalvm-ee-java11-20.0.0/Contents/Home/bin $PATH
 set -gx GRAALVM_HOME /Library/Java/JavaVirtualMachines/graalvm-ee-java11-20.0.0/Contents/Home
 
-set -gx PATH ~/.local/bin $PATH
+set -gx PATH ~/.deno/bin ~/.local/bin $PATH
