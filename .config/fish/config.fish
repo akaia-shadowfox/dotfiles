376f5169bb3a777a04333927f9b29d189fe79cd9
Karina Shedlbauer <k@rina.wtf>
2021-10-06 15:18:06 +0300

    Update fish configuration

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,7 +8,5 @@ source ~/.asdf/asdf.fish
 fenv source ~/.nix-profile/etc/profile.d/nix.sh
 
 set -gx EDITOR nvim
-# set -gx PATH /Library/Java/JavaVirtualMachines/graalvm-ee-java11-20.0.0/Contents/Home/bin $PATH
-# set -gx GRAALVM_HOME /Library/Java/JavaVirtualMachines/graalvm-ee-java11-20.0.0/Contents/Home
-set -gx PATH ~/.deno/bin ~/.local/bin ~/.vim/plugged/vim-iced/bin /Library/Java/JavaVirtualMachines/zulu-16.jdk/Contents/Home/bin $PATH
+set -gx GRAALVM_HOME /Library/Java/JavaVirtualMachines/graalvm-ee-java11-20.0.0/Contents/Home
 
