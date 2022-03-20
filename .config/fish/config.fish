cc1f835f422fa714adc1d24ce28ab5ad075b4a0b
Karina Shedlbauer <k@rina.wtf>
2022-03-20 20:08:41 +0400

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,7 +5,7 @@ end
 source ~/.asdf/asdf.fish
 
 # Setup nix package manager
-fenv source ~/.nix-profile/etc/profile.d/nix.sh
+# fenv source ~/.nix-profile/etc/profile.d/nix.sh
 
 set -gx EDITOR nvim
 set -gx GRAALVM_HOME /Library/Java/JavaVirtualMachines/graalvm-ee-java11-20.0.0/Contents/Home
