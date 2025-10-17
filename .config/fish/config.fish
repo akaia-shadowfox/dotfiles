86449ffbe319876fccdb4b263e3ea899b5b971bd
Carina | carina.akaia.org <cvo.akaia@gmail.com>
2025-10-17 21:54:55 +0000

    Drop support for Mise and portable Nix installations

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,4 @@
 if status is-interactive
-    mise activate fish --shims | source
     starship init fish | source
     zoxide init fish | source
     macchina --theme astatine
@@ -10,19 +9,3 @@ end
 
 set -gx GCM_CREDENTIAL_STORE gpg
 set -gx GPG_TTY (tty)
-set -gx OLLAMA_MODELS /home/ai/ollama/models
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /home/shadowfox/anaconda3/bin/conda
-    eval /home/shadowfox/anaconda3/bin/conda "shell.fish" hook $argv | source
-else
-    if test -f "/home/shadowfox/anaconda3/etc/fish/conf.d/conda.fish"
-        . "/home/shadowfox/anaconda3/etc/fish/conf.d/conda.fish"
-    else
-        set -x PATH /home/shadowfox/anaconda3/bin $PATH
-    end
-end
-# <<< conda initialize <<<
-
-fish_add_path -a /home/shadowfox/.foundry/bin
