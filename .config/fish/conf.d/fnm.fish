bb1227a916a9f507a46f465cd449f6b208e50f2b
Carina <cvo.akaia@gmail.com>
2025-10-18 11:04:49 +0400

    Update fnm config

--- .config/fish/conf.d/fnm.fish
+++ .config/fish/conf.d/fnm.fish
@@ -1,1 +1,7 @@
 fnm env --use-on-cd --shell fish | source
+
+#set FNM_PATH "/home/shadowfox/.local/share/fnm"
+#if [ -d "$FNM_PATH" ]
+#  set PATH "$FNM_PATH" $PATH
+#  fnm env --use-on-cd --shell fish | source
+#end
